eacdf349b4912cbb7cfb93f71f724f92f222068e
zhang <1ghoststories@gmail.com>
2018-10-15 16:13:56 +0800

    加入显示天气功能

--- app/src/main/java/com/example/simpleweather/MainActivity.java
+++ app/src/main/java/com/example/simpleweather/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.simpleweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,5 +13,11 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
